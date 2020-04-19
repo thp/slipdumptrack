c7e5208a4f585c56aa299149228151580db8fab1
Thomas Perl <m@thp.io>
2020-04-19 11:47:47 +0200

    MM2-style multiplayer

--- sliprender.py
+++ sliprender.py
@@ -25,6 +25,8 @@ pygame.display.init()
 w = 640
 h = 480
 
+WAYPOINT_DEBUG = False
+
 scr = pygame.display.set_mode((w, h), DOUBLEBUF | OPENGL)
 
 def find_tile_ctrl(d, tile):
@@ -151,155 +153,262 @@ def make_fg_list():
 
 fglist = make_fg_list()
 
-# Find first tile in the third layer (ships + waypoints)
-px, py = find_tile_ctrl(d, FIRST_SHIP_TILE)
+KEYMAP = {
+    'left':       [K_LEFT,  K_a, K_j, K_f],
+    'right':      [K_RIGHT, K_d, K_l, K_h],
+    'accelerate': [K_UP,    K_w, K_i, K_t],
+    'reverse':    [K_DOWN,  K_s, K_k, K_g],
+}
+
+class Player(object):
+    def __init__(self, idx):
+        self.idx = idx
+        self.reset()
+        self.gotcash = 0
+        self.points = 0
+
+    def reset(self):
+        # Find first tile in the third layer (ships + waypoints)
+        px, py = find_tile_ctrl(d, FIRST_SHIP_TILE+self.idx)
+        self.ppos = Vector2((px, py)) * ts
+        self.rotation = 0
+        self.speed = 0
+        self.ship_height = 2
+        self.ship_dheight = 0
+        self.lap = 0
+        self.next_waypoint = 0
+        self.steer = 0
+        self.accelerate = False
+        self.reverse = False
+        self.celebrating = False
+        self.alive = True
+
+    def stop_moving(self):
+        self.accelerate = False
+        self.reverse = False
+        self.speed = 0
+        self.steer = 0
+
+    def start_celebrating(self):
+        self.celebrating = True
+        self.points += 1
+        self.stop_moving()
+
+    def kill_off(self):
+        self.alive = False
+        self.stop_moving()
+
+    def __repr__(self):
+        return f'<Player {self.idx} @ ({self.ppos.x}, {self.ppos.y}) alive={self.alive}>'
+
+    def handle(self, evt):
+        if evt.type == KEYDOWN:
+            if evt.key == KEYMAP['left'][self.idx]:
+                self.steer = -3
+            elif evt.key == KEYMAP['right'][self.idx]:
+                self.steer = +3
+            elif evt.key == KEYMAP['accelerate'][self.idx]:
+                self.accelerate = True
+            elif evt.key == KEYMAP['reverse'][self.idx]:
+                self.reverse = True
+        elif evt.type == KEYUP:
+            if evt.key == KEYMAP['left'][self.idx] or evt.key == KEYMAP['right'][self.idx]:
+                self.steer = 0
+            elif evt.key == KEYMAP['accelerate'][self.idx]:
+                self.accelerate = False
+            elif evt.key == KEYMAP['reverse'][self.idx]:
+                self.reverse = False
+
+    def get_waypoint_distances(self):
+        return [(math.sqrt((self.ppos.x-x*16)**2+(self.ppos.y-y*16)**2), i, x, y)
+                for i, (x, y) in enumerate(waypoints)]
+
+    def get_distance_to_next(self):
+        waypoint_distances = self.get_waypoint_distances()
+        return next(dist for (dist, i, x, y) in waypoint_distances if i == self.next_waypoint)
+
+
+PLAYERS = 4
+
+players = [Player(i) for i in range(PLAYERS)]
 
 waypoints = [find_tile_ctrl(d, FIRST_WAYPOINT_TILE+i) for i in range(8)]
 
-ppos = Vector2((px, py)) * ts
-steer = 0
-rotation = 0
-zoomin = 1
-speed = 0
-maxspeed = 6
-accelerate = False
-reverse = False
-gotcash = 0
-next_waypoint = 0
-lap = 0
-player_celebrating = False
-player_ship_height = 2
-player_ship_dheight = 0
+zoomin = 2
+MAXSPEED = 6
 
 def collides_with(ppos, x, y):
     return math.sqrt((x-ppos.x)**2 + (y-ppos.y)**2) < ts
+
+camx = 0
+camy = 0
+
+celebration_started = None
 
 while True:
     while True:
         evt = pygame.event.poll()
         if evt.type == NOEVENT:
             break
-        elif evt.type == KEYDOWN:
-            if evt.key == K_ESCAPE:
+
+        if evt.type == KEYDOWN:
+            if evt.key in (K_ESCAPE, K_q):
                 raise SystemExit(1)
-            if evt.key == K_LEFT:
-                steer = -3
-            elif evt.key == K_RIGHT:
-                steer = +3
-            elif evt.key == K_UP:
-                accelerate = True
-            elif evt.key == K_DOWN:
-                reverse = True
-            elif evt.key == K_SPACE:
-                player_celebrating = not player_celebrating
-        elif evt.type == KEYUP:
-            if evt.key == K_LEFT or evt.key == K_RIGHT:
-                steer = 0
-            elif evt.key == K_UP:
-                accelerate = False
-            elif evt.key == K_DOWN:
-                reverse = False
+
+        if not any(player.celebrating for player in players):
+            for player in players:
+                if player.alive:
+                    player.handle(evt)
 
     glClear(GL_COLOR_BUFFER_BIT)
 
-    xo = int(ppos.x + ts/2 - w/2/zoomin)
-    yo = int(ppos.y + ts/2 - h/2/zoomin)
+    alive_players = [player for player in players if player.alive]
+
+    # Check winning condition (one screenful distance)
+    max_x_distance = w/zoomin
+    max_y_distance = h/zoomin
+    min_players_x = min(player.ppos.x for player in alive_players)
+    max_players_x = max(player.ppos.x for player in alive_players)
+    min_players_y = min(player.ppos.y for player in alive_players)
+    max_players_y = max(player.ppos.y for player in alive_players)
+    if max_players_x - min_players_x > max_x_distance or max_players_y - min_players_y > max_y_distance:
+        ranked_alive_players = sorted(alive_players, key=lambda player: (player.lap, player.next_waypoint, -player.get_distance_to_next()))
+        print('killing off:', ranked_alive_players[0])
+        ranked_alive_players[0].kill_off()
+
+    alive_players = [player for player in players if player.alive]
+
+    if len(alive_players) == 1 and celebration_started is None:
+        alive_players[0].start_celebrating()
+        celebration_started = time.time()
+
+    if celebration_started is not None:
+        if time.time() - celebration_started > 5:
+            celebration_started = None
+            for player in players:
+                player.reset()
+
+    xo = int((min_players_x + max_players_x) / 2 + ts/2 - w/2/zoomin)
+    yo = int((min_players_y + max_players_y) / 2 + ts/2 - h/2/zoomin)
+
+    alpha = 0.8
+
+    camx = camx * alpha + xo * (1 - alpha)
+    camy = camy * alpha + yo * (1 - alpha)
+
     glLoadIdentity()
-    glOrtho(xo, xo+int(w/zoomin), yo+int(h/zoomin), yo, 0, 1)
+    glOrtho(int(camx), int(camx+w/zoomin), int(camy+h/zoomin), int(camy), 0, 1)
 
     # Draw background below
     glCallList(bglist)
+
+    # TODO: Player<->Player Collision
 
     # Draw cash + cash collision
     glBegin(GL_QUADS)
     glColor4f(1., 1., 1., 1.)
     for x, y, ch in list(cashes):
-        if collides_with(ppos, x*ts, y*ts):
-            gotcash += 1
-            cashes.remove((x, y, ch))
+        for player in players:
+            if collides_with(player.ppos, x*ts, y*ts):
+                player.gotcash += 1
+                cashes.remove((x, y, ch))
+                break
         blitquad(x, y, FIRST_CASH_TILE + int(time.time()*8) % (LAST_CASH_TILE - FIRST_CASH_TILE + 1))
 
     # render player ship (with shadow)
-    glColor4f(1., 1., 1., 1. / (1 + player_ship_height/4))
-    blitquad(int(ppos.x)/ts, int(ppos.y)/ts, FIRST_SHIP_SHADOW_TILE, rotation, 1. + (player_ship_height-2) / 32)
-    glColor4f(1., 1., 1., 1.)
-    blitquad(int(ppos.x-player_ship_height/2)/ts, int(ppos.y-player_ship_height/2)/ts, FIRST_SHIP_TILE, rotation)
+    for player in players:
+        if not player.celebrating:
+            glColor4f(1., 1., 1., 1. / (1 + player.ship_height/4))
+            blitquad(int(player.ppos.x)/ts, int(player.ppos.y)/ts, FIRST_SHIP_SHADOW_TILE+player.idx, player.rotation, 1. + (player.ship_height-2) / 32)
+            glColor4f(1., 1., 1., 1.)
+            blitquad(int(player.ppos.x-player.ship_height/2)/ts, int(player.ppos.y-player.ship_height/2)/ts, FIRST_SHIP_TILE+player.idx, player.rotation)
     glEnd()
 
-    waypoint_distances = [(math.sqrt((ppos.x-x*16)**2+(ppos.y-y*16)**2), i, x, y)
-                          for i, (x, y) in enumerate(waypoints)]
+    for player in players:
+        waypoint_distances = player.get_waypoint_distances()
 
-    for pos, (distance, i, x, y) in enumerate(sorted(waypoint_distances)):
-        glColor4f(1., 1., 1., 1.)
-        glBegin(GL_QUADS)
-        blitquad(x, y, FIRST_WAYPOINT_TILE+i)
-        glEnd()
+        for pos, (distance, i, x, y) in enumerate(sorted(waypoint_distances)):
+            glColor4f(1., 1., 1., 1.)
+            glBegin(GL_QUADS)
+            blitquad(x, y, FIRST_WAYPOINT_TILE+i)
+            glEnd()
 
-        glDisable(GL_TEXTURE_2D)
-        glBegin(GL_LINES)
-        if pos == 0:
-            if next_waypoint == i:
-                next_waypoint += 1
-                print(f'next waypoint reached {next_waypoint}')
+            glDisable(GL_TEXTURE_2D)
+            glBegin(GL_LINES)
+            if pos == 0:
+                if player.next_waypoint == i:
+                    player.next_waypoint += 1
+                    print(f'next waypoint reached {player.next_waypoint}')
 
-                if next_waypoint == len(waypoints):
-                    print('next lap')
-                    next_waypoint = 0
-                    lap += 1
+                    if player.next_waypoint == len(waypoints):
+                        print('next lap')
+                        player.next_waypoint = 0
+                        player.lap += 1
 
-            glColor4f(1., 1., 0., 1.)
-        else:
-            if next_waypoint == i:
-                glColor4f(0., 1., 0., 1.)
+                glColor4f(1., 1., 0., 1. if WAYPOINT_DEBUG else 0)
             else:
-                glColor4f(1., 1., 0., 0.1)
-        glVertex2f(x*16+8, y*16+8)
-        glVertex2f(ppos.x+8, ppos.y+8)
-        glEnd()
-        glEnable(GL_TEXTURE_2D)
+                if player.next_waypoint == i:
+                    glColor4f(0., 1., 0., 1. if WAYPOINT_DEBUG else 0)
+                else:
+                    glColor4f(1., 1., 0., 0.1 if WAYPOINT_DEBUG else 0)
+            glVertex2f(x*16+8, y*16+8)
+            glVertex2f(player.ppos.x+8, player.ppos.y+8)
+            glEnd()
+            glEnable(GL_TEXTURE_2D)
 
-    # Wall hit checks
-    hit = False
-    for x, y in walls:
-        if collides_with(ppos, x*ts, y*ts):
-            ppos -= Vector2(x*ts-ppos.x, y*ts-ppos.y)
-            speed *= -0.5
-            hit = True
+        # Wall hit checks
+        hit = False
+        for x, y in walls:
+            if collides_with(player.ppos, x*ts, y*ts):
+                player.ppos -= Vector2(x*ts-player.ppos.x, y*ts-player.ppos.y)
+                player.speed *= -0.5
+                hit = True
 
-    if player_celebrating:
-        rotation += 4
+        if player.celebrating:
+            player.rotation += 4
 
-    # Steering
-    rotation += steer
+        # Steering
+        player.rotation += player.steer
 
-    # Move if not wall hit
-    if not hit:
-        ppos += Vector2((0, -1)).rotate(rotation) * speed
+        # Move if not wall hit
+        if not hit:
+            player.ppos += Vector2((0, -1)).rotate(player.rotation) * player.speed
 
-    # Accelerate/reverse/decelerate
-    if accelerate:
-        speed = min(maxspeed, speed + .2)
-    elif reverse:
-        speed = max(-maxspeed, speed - .2)
-    else:
-        speed *= 0.9
-
-    # Height
-    player_ship_dheight -= 1
-    player_ship_height += player_ship_dheight
-    if player_ship_height <= 2:
-        if abs(player_ship_dheight) > 2:
-            player_ship_dheight *= -0.8
+        # Accelerate/reverse/decelerate
+        if player.accelerate:
+            player.speed = min(MAXSPEED, player.speed + .2)
+        elif player.reverse:
+            player.speed = max(-MAXSPEED, player.speed - .2)
         else:
-            if player_celebrating:
-                player_ship_dheight = 10
+            player.speed *= 0.9
+
+        # Height
+        player.ship_dheight -= 1
+        player.ship_height += player.ship_dheight
+        if player.ship_height <= 2:
+            if abs(player.ship_dheight) > 2:
+                player.ship_dheight *= -0.8
             else:
-                player_ship_dheight = 0
-        player_ship_height = 2
+                if player.celebrating:
+                    player.ship_dheight = 10
+                else:
+                    player.ship_dheight = 0
+            player.ship_height = 2
 
     # Draw foreground on top
     glCallList(fglist)
 
+    # render celebrating players on top of foreground
+    glBegin(GL_QUADS)
+    glColor4f(1., 1., 1., 1.)
+    for player in players:
+        if player.celebrating:
+            glColor4f(1., 1., 1., 1. / (1 + player.ship_height/4))
+            blitquad(int(player.ppos.x)/ts, int(player.ppos.y)/ts, FIRST_SHIP_SHADOW_TILE+player.idx, player.rotation, 1. + (player.ship_height-2) / 32)
+            glColor4f(1., 1., 1., 1.)
+            blitquad(int(player.ppos.x-player.ship_height/2)/ts, int(player.ppos.y-player.ship_height/2)/ts, FIRST_SHIP_TILE+player.idx, player.rotation)
+    glEnd()
+
     # update screen
-    pygame.display.set_caption(f'{fn} - cash: {gotcash} - lap: {lap}')
+    pygame.display.set_caption(f'{fn} - cash: {players[0].gotcash} - lap: {players[0].lap}')
     pygame.display.flip()
